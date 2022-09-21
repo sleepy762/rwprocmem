adb1d9daba5d1aa453ee6c826ab2c95754e1471f
sleepy762 <binelemental@gmail.com>
2022-09-21 23:53:06 +0300

    Removed code duplication in the function templates

--- include/Utils.h
+++ include/Utils.h
@@ -14,8 +14,23 @@ namespace Utils
     std::string JoinVectorOfStrings(const std::vector<std::string>& vec, int startIndex, 
             char joinChar);
 
-    // std::from_chars takes different arguments depending on if the type is integral or float
+
+    // These function templates are very thin wrappers around std::from_chars
+    // std::from_chars is actually unable to detect whether a number is in hex
+    // so we have to explicitly call the function with a base 16/hex argument
     template <std::integral T>
+    std::from_chars_result from_chars_hex(const char* start, const char* end, T& value)
+    {
+        return std::from_chars(start, end, value, 16);
+    }
+
+    template <std::floating_point T>
+    std::from_chars_result from_chars_hex(const char* start, const char* end, T& value)
+    {
+        return std::from_chars(start, end, value, std::chars_format::hex);
+    }
+
+    template <typename T>
     T StrToNumber(const std::string& dataString)
     {
         T dataValue = 0;
@@ -28,45 +43,9 @@ namespace Utils
         if (dataStringStart[0] == '0' && (dataStringStart[1] == 'x' || dataStringStart[1] == 'X'))
         {
             dataStringStart += 2; // Move past the '0x'
-            res = std::from_chars(dataStringStart, dataStringEnd, dataValue, 16);
+            res = Utils::from_chars_hex<T>(dataStringStart, dataStringEnd, dataValue);
         }
         else // Use generic conversion
-        {
-            res = std::from_chars(dataStringStart, dataStringEnd, dataValue);
-        }
-
-        // Error checking
-        if (res.ec == std::errc::invalid_argument)
-        {
-            throw std::invalid_argument("Invalid data.");
-        }
-        else if (res.ec == std::errc::result_out_of_range)
-        {
-            throw std::runtime_error("Result of data conversion is out of range for the given type.");
-        }
-        else if (res.ptr != dataStringEnd)
-        {
-            throw std::runtime_error("Failed to fully convert the given data to a number.");
-        }
-        return dataValue;
-    }
-
-    template <std::floating_point T>
-    T StrToNumber(const std::string& dataString)
-    {
-        T dataValue = 0;
-        std::from_chars_result res;
-
-        const char* dataStringStart = dataString.c_str();
-        const char* dataStringEnd = dataStringStart + dataString.size();
-
-        // It also needs help with floating point numbers
-        if (dataStringStart[0] == '0' && (dataStringStart[1] == 'x' || dataStringStart[1] == 'X'))
-        {
-            dataStringStart += 2; // Move past the '0x'
-            res = std::from_chars(dataStringStart, dataStringEnd, dataValue, std::chars_format::hex);
-        }
-        else
         {
             res = std::from_chars(dataStringStart, dataStringEnd, dataValue);
         }
