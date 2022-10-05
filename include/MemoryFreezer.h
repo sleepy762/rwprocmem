034b4b8768c055c25ebd6e6d01adafcf3122fef1
sleepy762 <binelemental@gmail.com>
2022-10-05 19:39:20 +0300

    Implemented most of the memory freezer

---
+++ include/MemoryFreezer.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <cstdint>
+#include <string>
+#include <sys/types.h>
+#include <vector>
+#include <list>
+#include "MemoryStructs.h"
+#include <mutex>
+
+struct FrozenMemAddress
+{
+    MemAddress memAddress;
+    bool enabled; // A flag which tells the program whether the address should be frozen
+    std::string type; // Only used for printing to the user
+    std::string dataStr; // Also only used for printing to the user (removes the need for a template)
+    std::vector<uint8_t> data; // The data which will be continually written to the address
+};
+
+class MemoryFreezer
+{
+public:
+    MemoryFreezer();
+    ~MemoryFreezer();
+
+    void AddAddress(MemAddress address, std::string& typeStr, std::string& dataStr,
+            std::vector<uint8_t>& data);
+    void RemoveAddress(size_t index);
+    void RemoveAllAddresses();
+
+    void EnableAddress(size_t index);
+    void DisableAddress(size_t index);
+    void EnableAllAddresses();
+    void DisableAllAddresses();
+
+    const std::list<FrozenMemAddress>& GetFrozenAddresses() const;
+    int GetEnabledAddressesAmount() const;
+
+    void SetPid(pid_t pid);
+
+private:
+    void StartThreadLoopIfNeeded();
+    void ThreadLoop();
+
+    int m_EnabledAddressesAmount;
+    bool m_ThreadRunning;
+
+    pid_t m_pid;
+    std::list<FrozenMemAddress> m_FrozenAddresses;
+    std::mutex m_MemoryFreezerMutex;
+};
+
---
+++ src/MemoryFreezer.cpp
@@ -0,0 +1,168 @@
+#include "MemoryFreezer.h"
+#include <cstdint>
+#include <stdexcept>
+#include <thread>
+#include <vector>
+#include "MemoryFuncs.h"
+
+MemoryFreezer::MemoryFreezer()
+{
+    this->m_EnabledAddressesAmount = 0;
+    this->m_ThreadRunning = false;
+    this->m_pid = 0;
+}
+
+MemoryFreezer::~MemoryFreezer() {}
+
+void MemoryFreezer::AddAddress(MemAddress address, std::string& typeStr, std::string& dataStr, 
+        std::vector<uint8_t>& data)
+{
+    FrozenMemAddress frozenAddr = { address, true, typeStr, dataStr, data };
+    this->m_FrozenAddresses.push_back(frozenAddr);
+    this->m_EnabledAddressesAmount += 1;
+
+    // We may need to start/restart the thread if there were no addresses before
+    // and a new address was added now
+    this->StartThreadLoopIfNeeded();
+}
+
+void MemoryFreezer::RemoveAddress(size_t index)
+{
+    if (index > this->m_FrozenAddresses.size())
+    {
+        throw std::runtime_error("Index out of bounds.");
+    }
+    else
+    {
+        auto iter = this->m_FrozenAddresses.cbegin();
+        std::advance(iter, index);
+
+        this->m_FrozenAddresses.erase(iter);
+        this->m_EnabledAddressesAmount -= 1;
+    }
+}
+
+void MemoryFreezer::RemoveAllAddresses()
+{
+    this->m_FrozenAddresses.clear();
+    this->m_EnabledAddressesAmount = 0;
+}
+
+void MemoryFreezer::EnableAddress(size_t index)
+{
+    if (index > this->m_FrozenAddresses.size())
+    {
+        throw std::runtime_error("Index out of bounds.");
+    }
+    else
+    {
+        auto iter = this->m_FrozenAddresses.begin();
+        std::advance(iter, index);
+
+        iter->enabled = true;
+        this->m_EnabledAddressesAmount += 1;
+
+        this->StartThreadLoopIfNeeded();
+    }
+}
+
+void MemoryFreezer::DisableAddress(size_t index)
+{
+    if (index > this->m_FrozenAddresses.size())
+    {
+        throw std::runtime_error("Index out of bounds.");
+    }
+    else
+    {
+        auto iter = this->m_FrozenAddresses.begin();
+        std::advance(iter, index);
+
+        iter->enabled = false;
+        this->m_EnabledAddressesAmount -= 1;
+    }
+}
+
+void MemoryFreezer::EnableAllAddresses()
+{
+    for (auto it = this->m_FrozenAddresses.begin(); it != this->m_FrozenAddresses.end(); it++)
+    {
+        // Only count (and enable) addresses which are currently disabled
+        if (!it->enabled)
+        {
+            it->enabled = true;
+            this->m_EnabledAddressesAmount += 1;
+        }
+    }
+    this->StartThreadLoopIfNeeded();
+}
+
+void MemoryFreezer::DisableAllAddresses()
+{
+    for (auto it = this->m_FrozenAddresses.begin(); it != this->m_FrozenAddresses.end(); it++)
+    {
+        it->enabled = false;
+    }
+    this->m_EnabledAddressesAmount = 0;
+}
+
+const std::list<FrozenMemAddress>& MemoryFreezer::GetFrozenAddresses() const
+{
+    return this->m_FrozenAddresses;
+}
+
+int MemoryFreezer::GetEnabledAddressesAmount() const
+{
+    return this->m_EnabledAddressesAmount;
+}
+
+void MemoryFreezer::SetPid(pid_t pid)
+{
+    this->m_pid = pid;
+    this->m_FrozenAddresses.clear();
+    this->m_EnabledAddressesAmount = 0;
+}
+
+void MemoryFreezer::StartThreadLoopIfNeeded()
+{
+    // Start a thread only if there is no thread running already and if there are enabled addresses
+    if (!this->m_ThreadRunning && this->m_EnabledAddressesAmount > 0)
+    {
+        this->m_ThreadRunning = true;
+
+        std::thread th(&MemoryFreezer::ThreadLoop, this);
+        th.detach();
+    }
+}
+
+// TODO: Add mutex locks
+// TODO: Add exception handling
+void MemoryFreezer::ThreadLoop()
+{
+    auto it = this->m_FrozenAddresses.cbegin();
+    while (true)
+    {
+        // Stopping condition
+        if (this->m_EnabledAddressesAmount <= 0)
+        {
+            break;
+        }
+
+        // Restart the iterator if it reached the end
+        if (it == this->m_FrozenAddresses.cend())
+        {
+            it = this->m_FrozenAddresses.cbegin();
+        }
+
+        // Only freeze enabled addresses
+        if (it->enabled)
+        {
+            std::vector<uint8_t> data = it->data; 
+            MemoryFuncs::WriteToProcessMemory(this->m_pid, it->memAddress.address, data.size(), (void*)&data[0]);
+        }
+        
+        // Move forward
+        std::advance(it, 1);
+    }
+    this->m_ThreadRunning = false;
+}
+
